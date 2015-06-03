d1369b8f38689136e2398173a5f406e54e9bcf83
kalle (jag) <kalle.jillheden@me.com>
2015-06-03 21:42:36 +0200

    Basic commit
    Added Hammer
    
    Added script to control the hammer. Currently moves the
    hammer and
    activates its animation when you click. Also added hammer
    prefab,
    meshes, and basic swinging animations.
    - Added
    HammerController.cs
    - Added Hammer prefab
    - Added lambert1.mat basic
    material
    - Added Hammer.mb maya scene containing hammer meshes
    
    Added basic
    folders
    
    Assets/Scripts
    Assets/Prefabs
    Assets/Meshes
    Assets/Materials
    Assets/Animations
    Assets/Animations/Controllers

---
+++ Assets/Scripts/HammerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HammerController : MonoBehaviour {
+
+	public LayerMask raycastMask;
+	private Vector3 point;
+
+	private Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		Raycast ();
+
+		transform.position = point;
+	}
+
+	void Raycast() {
+		// Variables
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		// Cast the ray
+		if(Physics.Raycast (ray, out hit, Mathf.Infinity, raycastMask)) {
+			HandleHit(hit);
+		}
+	}
+
+	void HandleHit(RaycastHit hit) {
+		if (Input.GetMouseButtonDown (0)) {
+			anim.SetTrigger("Swing");
+		}
+
+		point = hit.point;
+	}
+
+}
