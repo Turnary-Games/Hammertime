a0c21f4a9bc6f1f197efda233fc28aec8fcd665b
Bejno <bejnofalk@gmail.com>
2015-06-04 06:15:51 +0200

    Added Decoration
    -Added Coins and logs to the map

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class Rotate : MonoBehaviour 
 {
-	
-	// Update is called once per frame
+	public float speed; 
+
 	void Update () 
 	{
-		transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime);
+		transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime * speed);
 	}
 }
